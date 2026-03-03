7ce580ef3aeb29971411817b93706eeedfe055fb
G-Lungov <gabriellungov2001@gmail.com>
2026-03-03 17:19:04 -0300

    SQL script structured

---
+++ src/database/script.sql
@@ -0,0 +1,27 @@
+USE education_one;
+
+    CREATE TABLE students (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(150) NOT NNULL,
+        biRth_date DATE NOT NULL,
+        cpf VARCHAR(11) NOT NULL UNIQUE,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+
+    CREATE TABLE classrooms (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        description VARCHAR(100) NOT NULL,
+        year INT NOT NULL,
+        places INT NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+
+    CREATE TABLER enrollments (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        student_id INT NOT NULL,
+        classroom_id INT NOT NULL,
+        enrollment_date DATE NOT NULL,
+        UNIQUE (student_id),
+        FOREIGN KEY (student_id) REFERENCES students(id),
+        FOREIGN KEY (classroom_id) REFERENCES classrooms(id)
+    );
